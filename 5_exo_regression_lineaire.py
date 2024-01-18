9a1abf115f9c9969ecf0d5dd74e520bd6698a4bd
Pierre Marboutin <piballz@gmail.com>
2024-01-18 15:16:42 +0100

    added natural ln

--- 5_exo_regression_lineaire.py
+++ 5_exo_regression_lineaire.py
@@ -1,6 +1,7 @@
-
+import math
 # t (time) in s
 t = [0, 2, 4, 6, 8, 10, 12, 14]
+N = [2, 5, 16, 20, 40, 100, 200, 300]
 
 
 def sum_array(arr):
@@ -13,8 +14,9 @@ def sum_array(arr):
 def average(arr):
     return sum_array(arr)/len(arr)
 
-
-print(f"La moyenne est : {average(t)}")
+print("\nPour t :")
+print("--------")
+print(f"- La moyenne est : {average(t)}")
 
 
 def square(arr):
@@ -24,10 +26,25 @@ def square(arr):
     return result
 
 
-
 def variance(arr):
-    print(square(arr))
-    print(average(arr))
     return square(arr)/len(arr)-(average(arr)*average(arr))
 
-print(variance(t))
+
+print(f"- La variance est : {variance(t)}")
+
+
+def natural_logarithm(arr):
+    ln = []
+    for elt in arr:
+        ln.append(round(math.log(elt), 8))
+    return ln
+
+print("\nPour ln(N) :")
+print("-------------")
+print(f"\n- La moyenne des ln est : {round(average(natural_logarithm(N)), 2)}")
+
+print(square(natural_logarithm(N))/8 - round(average(natural_logarithm(N)), 2)*round(average(natural_logarithm(N)), 2))
+
+
+# print(f" {variance(nat_logarithm )} ")
+14,48
