0c7032c410f2230ca9959954f25a4fac3d88a51c
Pierre Marboutin <piballz@gmail.com>
2024-01-22 12:24:18 +0100

    added fill start green

--- 15_stars.py
+++ 15_stars.py
@@ -12,9 +12,12 @@ branch_length = 100
 
 # Draw 1
 my_art.color("red")
+my_art.fillcolor("red")
+my_art.begin_fill()
 for i in range(5):
     my_art.forward(branch_length)
     my_art.right(144)
+
 
 
 my_art.penup()
@@ -23,12 +26,16 @@ my_art.pendown()
 
 # Draw 2
 my_art.color("green")
+my_art.fillcolor("green")
+my_art.begin_fill()
 branch_length = 50
 for i in range(5):
     for j in range(2):
         my_art.forward(branch_length)
         my_art.left(72)
     my_art.left(144)
+
+my_art.end_fill()
 
 
 turtle.exitonclick()
