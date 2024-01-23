48ea5ed3c4b790a23202554f1e835b3d99da891a
Pierre Marboutin <piballz@gmail.com>
2024-01-23 10:16:53 +0100

    added exercise match refacto

--- 16_exercise_match.py
+++ 16_exercise_match.py
@@ -1,19 +1,24 @@
 # Créer une fonction qui vérifie la lettre entréé par l'utilisateur est une voyelle
 
-letter_entry = input("Saisir une lettre : ")
+letter_entry = ''
 
-match str(letter_entry):
-    case 'a':
-        print('Cette lettre  {letter_entry} est une voyelle')
-    case 'e':
-        print('Cette lettre  {letter_entry} est une voyelle')
-    case 'i':
-        print('Cette lettre  {letter_entry} est une voyelle')
-    case 'o':
-        print('Cette lettre  {letter_entry} est une voyelle')
-    case 'u':
-        print('Cette lettre  {letter_entry} est une voyelle')
-    case 'y':
-        print('Cette lettre  {letter_entry} est une voyelle')
-    case _:
-        print(f"Cette lettre {letter_entry} n'est pas une voyelle")
+def vowel_verifcator():
+    letter_entry = input("Saisir une lettre : ")
+    match str(letter_entry):
+        case 'a':
+            print(f'Cette lettre {letter_entry} est une voyelle')
+        case 'e':
+            print(f'Cette lettre {letter_entry} est une voyelle')
+        case 'i':
+            print(f'Cette lettre {letter_entry} est une voyelle')
+        case 'o':
+            print(f'Cette lettre {letter_entry} est une voyelle')
+        case 'u':
+            print(f'Cette lettre {letter_entry} est une voyelle')
+        case 'y':
+            print(f'Cette lettre {letter_entry} est une voyelle')
+        case _:
+            print(f"Cette lettre {letter_entry} n'est pas une voyelle")
+
+
+vowel_verifcator()
