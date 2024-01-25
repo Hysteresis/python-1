4d4b50d99eb5e682df42e776c92a7965d487e39d
Pierre Marboutin <piballz@gmail.com>
2024-01-25 18:08:56 +0100

    fixed var url

--- boite_du_fromager/main.py
+++ boite_du_fromager/main.py
@@ -3,5 +3,5 @@ from extract import Extract
 
 url = "https://www.laboitedufromager.com/liste-des-fromages-par-ordre-alphabetique/"
 
-extractor = Extract("https://www.laboitedufromager.com/liste-des-fromages-par-ordre-alphabetique/")
+extractor = Extract(url)
 extractor.read_website()
